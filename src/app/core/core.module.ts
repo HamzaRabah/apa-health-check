1fa811330de3d9f69eae5bca7e7cd923f21592d4
Hamza Rabah <h.rabah@outlook.com>
2022-06-13 15:48:36 +0300

    Remove flex-layout module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HttpClientModule} from "@angular/common/http";
 import { HeaderComponent } from './components/header/header.component';
-import {FlexLayoutModule} from "@angular/flex-layout";
 import {MatToolbarModule} from "@angular/material/toolbar";
 
 
@@ -12,12 +11,10 @@ import {MatToolbarModule} from "@angular/material/toolbar";
   ],
   exports: [
     HeaderComponent,
-    FlexLayoutModule,
   ],
   imports: [
     CommonModule,
     HttpClientModule,
-    FlexLayoutModule,
     MatToolbarModule,
 
   ]
