13e86671e6aa6b7e2699437e9f24636f41a136f7
Hamza Rabah <h.rabah@outlook.com>
2022-11-08 15:55:51 +0300

    fix

--- netlify/functions/auth/auth.ts
+++ netlify/functions/auth/auth.ts
@@ -27,12 +27,10 @@ export const handler: Handler = async (event, context) => {
       console.log(tokenObject.token)
       console.log("tokenObjectAS")
       console.log(tokenObject as AccessToken)
-      let token = AuthService.authInstance().createToken(tokenObject.token);
+      let token = AuthService.authInstance().createToken(tokenObject);
       console.log("token")
-      console.log(token)
-      // if (token.expired(EXPIRATION_WINDOW_IN_SECONDS)) {
+      if (token.expired(EXPIRATION_WINDOW_IN_SECONDS)) {
       console.log('token expired')
-      console.log(token)
       console.log('refreshing token')
       try {
         const refreshParams = {};
@@ -46,7 +44,7 @@ export const handler: Handler = async (event, context) => {
 
       console.log('refreshing refreshed')
       console.log(token)
-      // }
+      }
       URI = AuthService.createResultURL(token, redirectUrl, csrfToken)
     }
   }
